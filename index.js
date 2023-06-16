062985c68b608b4dc47f5a38417c0527e334fee8
cgordon1029 <cjgordon1029@gmail.com>
2023-06-16 11:51:02 -0400

    EmailV

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function ValidateEmail(email){ //ValidateEmail
 	var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;//makes sure if it is a actual email address
-	
+
 		if (input.value.match(validRegex)) {
 			alert("Valid email address!");
 			document.form1.text1.focus();
